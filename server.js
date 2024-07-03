2a591fd473044efca42542e24ab58c60fc64c478
AzulitePoke <71411978+AlexErrand@users.noreply.github.com>
2024-07-03 05:14:49 -0500

    restructuring of files and sprite functionality

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the HTML file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Endpoint to fetch raw data
@@ -83,6 +87,7 @@ async function getJSON(Year, Month, Format, Rating) {
     const json = await fetchAPI(url);
     return json;
 }
+
 
 /*
 function updateSprites() {
